97dde537a4f2d68599df64fb12f40bb272105921
nayanab <nayanabm.94@gmail.com>
2016-11-15 17:29:28 +0530

    ,..

--- trial.R
+++ trial.R
@@ -2,7 +2,7 @@ library(RPostgreSQL)
 drv <- dbDriver("PostgreSQL")
 db <- dbConnect(drv, dbname="postgres", host= "localhost", port=5432,  user="postgres")
 
-q = "update assetdata set neteffect = 0.4 where asset_id = 1;"
+q = paste("update assetdata set neteffect = 0.5 where asset_id ="," 1;",sep="")
   print(dbGetQuery(db, q))
 
 dbDisconnect(db)
