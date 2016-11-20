1ceae7ee03d5529aa1d5fc72da5ae178802be8c1
nayanabm <bnayana@thoughtworks.com>
2016-11-20 09:02:58 +0530

    Delete trial.R

--- trial.R
+++
@@ -1,9 +0,0 @@
-library(RPostgreSQL)
-drv <- dbDriver("PostgreSQL")
-db <- dbConnect(drv, dbname="postgres", host= "localhost", port=5432,  user="postgres")
-
-q = paste("update assetdata set neteffect = 0.5 where asset_id ="," 1;",sep="")
-  print(dbGetQuery(db, q))
-
-dbDisconnect(db)
-dbUnloadDriver(drv)
