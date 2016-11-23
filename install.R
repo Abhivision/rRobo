0226ccf222bcd1611c0d231deacb2985146011ed
nayanabm <bnayana@thoughtworks.com>
2016-11-23 23:59:33 +0530

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,16 @@
+if(!require("RPostgreSQL")){
 install.packages("RPostgreSQL",dependencies=TRUE,repos="http://cran.rstudio.com/")
+}
+if(!require("data.table")){
 install.packages("data.table",dependencies=TRUE,repos="http://cran.rstudio.com/")
+}
+if(!require("reshape")){
 install.packages("reshape",dependencies=TRUE,repos="http://cran.rstudio.com/")
+}
+if(!require("igraph")){
 install.packages("igraph",dependencies=TRUE,repos="http://cran.rstudio.com/")
+}
+if(!require("jsonlite")){
 install.packages("jsonlite",dependencies=TRUE,repos="http://cran.rstudio.com/")
+}
+
